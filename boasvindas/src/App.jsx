c7e29c8d86ea9efb1aedb60e465980f03b04454d
maferssie <maria.evangelista23@etec.sp.gov.br>
2025-04-08 12:24:16 -0300

    08/04

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,23 @@
+import BoasVindas from "./Exemplo1"
+
+function App() {
+  
+  return (
+    <div>
+      <h1> Boas Vindas ao React com JavaScript</h1>
+
+      <p>
+        Este é o arquivo App.jsx que será o nosso ponto de partida para o desenvolvimento de aplicação Raect utilizando JavaScript.
+      </p>
+
+      <BoasVindas nome="Maria Fernanda"/>
+      <BoasVindas nome="Laís"/>
+      <BoasVindas nome="Anna Luiza"/>
+      <BoasVindas nome="Lorena"/>
+      <BoasVindas nome="Fernanda"/>
+
+    </div>
+  )
+}
+
+export default App
